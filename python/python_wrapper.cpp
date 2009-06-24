b37fdfcc06ac6bcffe7d1ef9a261f6c42d58a7fc
danny <danny@3a7aa2ed-2c50-d14e-a1b9-f07bebb4988c>
2009-06-24 18:42:33 +0000

    got some first python bindings working

--- python/python_wrapper.cpp
+++ python/python_wrapper.cpp
@@ -1,13 +1,42 @@
 #include <boost/python.hpp>
+#include "listing_parser.hpp"
+#include "sx_simulator.hpp"
+
+#include <string>
+#include <iostream>
+#include <fstream>
 
 char const* greet()
 {
    return "hello, world";
 }
 
+listing_info ParseListingFromFilename( const std::string &name)
+{
+	std::ifstream file( name.c_str());
+	if (!file)
+	{
+		throw std::runtime_error("file does not exist");
+	}
+
+	return ParseListingFile( file);
+}
+
+bool load_rom( sx_simulator &sim, const listing_info &listing)
+{
+	return sim.load_rom( listing.instructions);
+}
+
 BOOST_PYTHON_MODULE(pysix)
 {
     using namespace boost::python;
-    def("greet", greet);
+    class_<listing_info>("ListingInfo");
+
+    def( "ParseListingFile", ParseListingFromFilename);
+
+    class_<sx_simulator>("Simulator")
+		.def( "load_rom", &load_rom)
+		.def( "run", &sx_simulator::run)
+		.def( "reset", &sx_simulator::reset);
 }
 
